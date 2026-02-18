33b3c62253e9558a77fe90e320d725dbbf71a51c
PinkytheEbunny <JTaylor080910@gmail.com>
2026-02-18 15:52:26 +0000

    COFODFIOFIOEFIEF

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,3 +1,12 @@
-row1 = [0, 0, 0]
-row2 = [0, 0, 0]
-row3 = [0, 0, 0]
+sqaures = []
+def is_odd():
+    for i in range(100):
+        sqaures.append(i**2)
+    odd_numbers = [num for num in sqaures if num % 2 != 0]
+    total_sum = sum(sqaures)
+    print(total_sum)
+is_odd()
+
+
+
+
