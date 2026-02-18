6b3c5dc588c4350e9d8cbc1d9133b64c84fed465
WashedAtCoding <Thecozyshark@gmail.com>
2026-02-18 15:00:48 +0000

    hbkjhbkjhbkhb

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 print('GOON')
 print('hello')
 print("MEWOEOIWOEINOWNEFOWIENFOin")
 print("hello twin')
 print("new line')
+=======
+
+print('hello twin')
+>>>>>>> 9d380c3 (hbkjhbkjhbkhb)
